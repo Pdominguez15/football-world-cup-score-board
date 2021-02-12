a51f141c02fa967f11955a67c1d95af6907d7455
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-02-12 20:49:32 +0100

    Test for board component

---
+++ src/pods/board/board.component.spec.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import { BoardComponent } from "./board.component";
+
+describe("pods/board/board.component specs", () => {
+  it("should display a game when it is created", () => {
+    // Arrange
+    const onCreate = (homeTeam: string, awayTeam: string) => {};
+    const liveGames = [
+      {
+        id: 1,
+        homeTeam: "Home team test",
+        homeScore: 0,
+        awayTeam: "Away team test",
+        awayScore: 0,
+        updateDate: 1234,
+      },
+    ];
+    const summaryGames = [];
+    // Act
+    render(<BoardComponent onCreate={onCreate} liveGames={liveGames} />);
+
+    const game = screen.getByRole("listitem");
+    // Assert
+    expect(game).toBeInTheDocument();
+    expect(game).toContainHTML(
+      '<li>Home team test 0 - Away team test 0 <button name="update">Update game</button> <button name="finish">Finish game</button></li>'
+    );
+  });
+});
