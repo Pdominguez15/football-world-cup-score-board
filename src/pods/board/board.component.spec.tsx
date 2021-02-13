409dc11c61aa812b733c628d6f214ad4c308352c
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-02-13 11:24:45 +0100

    Add new test for board component

--- src/pods/board/board.component.spec.tsx
+++ src/pods/board/board.component.spec.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BoardComponent } from "./board.component";
 
 describe("pods/board/board.component specs", () => {
@@ -25,6 +26,60 @@ describe("pods/board/board.component specs", () => {
     expect(game).toBeInTheDocument();
     expect(game).toContainHTML(
       '<li>Home team test 0 - Away team test 0 <button name="update">Update game</button> <button name="finish">Finish game</button></li>'
+    );
+  });
+  it("should display a updated game when it is updated", () => {
+    // Arrange
+    const onCreate = (homeTeam: string, awayTeam: string) => {};
+    const onUpdate = (id: number, homeScore: number, awayScore: number) => {
+      liveGames.find((game) => {
+        if (game.id === id) {
+          game.homeScore = homeScore;
+          game.awayScore = awayScore;
+        }
+      });
+    };
+    let liveGames = [
+      {
+        id: 1,
+        homeTeam: "Home team test",
+        homeScore: 0,
+        awayTeam: "Away team test",
+        awayScore: 0,
+        createDate: 1234,
+      },
+    ];
+    // Act
+    render(
+      <BoardComponent
+        onCreate={onCreate}
+        onUpdate={onUpdate}
+        liveGames={liveGames}
+      />
+    );
+
+    const updateGame = screen.getByRole("button", { name: /Update game/ });
+    userEvent.click(updateGame);
+
+    const scoreHome = screen.getByRole("spinbutton", {
+      name: /Home team test/,
+    });
+    userEvent.type(scoreHome, "2");
+
+    const scoreAway = screen.getByRole("spinbutton", {
+      name: /Away team test/,
+    });
+    userEvent.type(scoreAway, "3");
+
+    const updateGame2 = screen.getByRole("button", { name: /Update game/ });
+    userEvent.click(updateGame2);
+
+    const game = screen.getByRole("listitem");
+
+    // Assert
+    expect(game).toBeInTheDocument();
+    expect(game).toContainHTML(
+      '<li>Home team test 2 - Away team test 3 <button name="update">Update game</button> <button name="finish">Finish game</button></li>'
     );
   });
 });
