def9f435e4dd75d96b7816f1ca437e5330acb393
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-02-13 12:17:31 +0100

    Add new test for board component

--- src/pods/board/board.component.spec.tsx
+++ src/pods/board/board.component.spec.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BoardComponent } from "./board.component";
+import { Game } from "./game.vm";
 
 describe("pods/board/board.component specs", () => {
   it("should display a game when it is created", () => {
@@ -128,5 +129,125 @@ describe("pods/board/board.component specs", () => {
 
     // Assert
     expect(game).not.toBeInTheDocument();
+  });
+  it("should display a summary of games order by score", () => {
+    // Arrange
+    const onCreate = (homeTeam: string, awayTeam: string) => {};
+    const onUpdate = (id: number, homeScore: number, awayScore: number) => {};
+    const onFinish = (id: number) => {};
+    const liveGames = [];
+    const summaryGames = [
+      {
+        id: 0,
+        homeTeam: "Home team test",
+        homeScore: 0,
+        awayTeam: "Away team test",
+        awayScore: 0,
+        createDate: 1234,
+      },
+      {
+        id: 1,
+        homeTeam: "Home team test1",
+        homeScore: 1,
+        awayTeam: "Away team test1",
+        awayScore: 0,
+        createDate: 1235,
+      },
+    ];
+    summaryGames.sort((gameA: Game, gameB: Game) => {
+      const totalScoreA = gameA.homeScore + gameA.awayScore;
+      const totalScoreB = gameB.homeScore + gameB.awayScore;
+      if (totalScoreA > totalScoreB) {
+        return -1;
+      }
+      if (totalScoreA === totalScoreB) {
+        if (gameA.createDate > gameB.createDate) {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        return 1;
+      }
+    });
+    // Act
+    render(
+      <BoardComponent
+        onCreate={onCreate}
+        onUpdate={onUpdate}
+        onFinish={onFinish}
+        liveGames={liveGames}
+        summaryGames={summaryGames}
+      />
+    );
+
+    const game = screen.getAllByRole("listitem");
+    // Assert
+    expect(game[0]).toContainHTML(
+      "<li>Home team test1 1 - Away team test1 0</li>"
+    );
+    expect(game[1]).toContainHTML(
+      "<li>Home team test 0 - Away team test 0</li>"
+    );
+  });
+  it("should display a summary of games order by date if both have the same score", () => {
+    // Arrange
+    const onCreate = (homeTeam: string, awayTeam: string) => {};
+    const onUpdate = (id: number, homeScore: number, awayScore: number) => {};
+    const onFinish = (id: number) => {};
+    const liveGames = [];
+    const summaryGames = [
+      {
+        id: 0,
+        homeTeam: "Home team test",
+        homeScore: 2,
+        awayTeam: "Away team test",
+        awayScore: 2,
+        createDate: 1234,
+      },
+      {
+        id: 1,
+        homeTeam: "Home team test1",
+        homeScore: 2,
+        awayTeam: "Away team test1",
+        awayScore: 2,
+        createDate: 1235,
+      },
+    ];
+    summaryGames.sort((gameA: Game, gameB: Game) => {
+      const totalScoreA = gameA.homeScore + gameA.awayScore;
+      const totalScoreB = gameB.homeScore + gameB.awayScore;
+      if (totalScoreA > totalScoreB) {
+        return -1;
+      }
+      if (totalScoreA === totalScoreB) {
+        if (gameA.createDate > gameB.createDate) {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        return 1;
+      }
+    });
+    // Act
+    render(
+      <BoardComponent
+        onCreate={onCreate}
+        onUpdate={onUpdate}
+        onFinish={onFinish}
+        liveGames={liveGames}
+        summaryGames={summaryGames}
+      />
+    );
+
+    const game = screen.getAllByRole("listitem");
+    // Assert
+    expect(game[0]).toContainHTML(
+      "<li>Home team test 2 - Away team test 2</li>"
+    );
+    expect(game[1]).toContainHTML(
+      "<li>Home team test1 2 - Away team test1 2</li>"
+    );
   });
 });
